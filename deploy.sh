f8b2aaa62491eb097b21e9ccd014231a48c9b05a
Chris Holden <ceholden@gmail.com>
2014-09-19 12:30:09 -0400

    Deploy script now patches out Cython usage for YATSM plugin

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,13 @@ fi
 
 set -e
 
+# Patch out Cython in YATSM if it exists
+yatsm=src/yatsm/yatsm/yatsm.py
+if [ -f $yatsm ]; then
+    sed -i 's|from cyatsm import|#from cyatsm import|g' $yatsm
+    sed -i 's|cymultitemp_mask|multitemp_mask|g' $yatsm
+fi
+
 make
 make clean
 make derase
