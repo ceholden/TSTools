e52984fd4aeb6ef385c5ba1e93d536bc045dc6ed
Chris Holden <ceholden@gmail.com>
2015-06-24 15:38:46 -0400

    add code to parse Landsat MTL files to utils

--- src/ts_driver/ts_utils.py
+++ src/ts_driver/ts_utils.py
@@ -107,3 +107,26 @@ def set_custom_config(obj, values):
                 'because types do not match ({t1} vs. {t2})'
                 .format(v=val, o=attr, cv=current_val,
                         t1=type(val), t2=type(current_val)))
+
+
+def parse_landsat_MTL(mtl_file, key):
+    """ Returns the value of specified key for a given Landsat MTL file
+
+    Args:
+      mtl_file (str): filename of MTL file
+      key (str): metadata key to search for
+
+    Returns:
+      str or int: returns integer representation of value if possible, else
+        as a string
+
+    """
+    with open(mtl_file, 'rb') as f:
+        for line in f:
+            if key in line:
+                value = line.strip().split('=')[1].strip()
+                try:
+                    value = int(value)
+                    return value
+                except:
+                    return value
