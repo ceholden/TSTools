3ffdf7395fc359ebf66ff95d800a1cb1bbfddbd1
Chris Holden <ceholden@gmail.com>
2015-06-25 14:52:04 -0400

    Update plugin to v0.99; remove depricated metadata

--- src/__init__.py
+++ src/__init__.py
@@ -1,54 +1,5 @@
-# -*- coding: utf-8 -*-
+""" TSTools QGIS Plugin: __init__.py
 """
-/***************************************************************************
- TSTools
-                                 A QGIS plugin
- Plugin for visualization and analysis of remote sensing time series
-                             -------------------
-        begin                : 2013-10-01
-        copyright            : (C) 2013 by Chris Holden
-        email                : ceholden@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
-"""
-
-
-def name():
-    return "TSTools"
-
-
-def description():
-    return "Plugin for visualization and analysis \
-            of remote sensing time series"
-
-
-def version():
-    return "Version 0.1"
-
-
-def icon():
-    return "icon.png"
-
-
-def qgisMinimumVersion():
-    return "2.0"
-
-
-def author():
-    return "Chris Holden"
-
-
-def email():
-    return "ceholden@gmail.com"
 
 
 def classFactory(iface):
