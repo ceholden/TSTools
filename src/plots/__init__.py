d951eec6e1bd5f1a91388597ecf769265c3f9485
Chris Holden <ceholden@gmail.com>
2015-07-06 12:20:25 -0500

    Implement DOY plot; TODO results & picker

--- src/plots/__init__.py
+++ src/plots/__init__.py
@@ -4,4 +4,4 @@ from .plot_doy import DOYPlot
 from .plot_ts import TSPlot
 
 # plots = [TSPlot, DOYPlot]
-plots = [TSPlot]
+plots = [TSPlot, DOYPlot]
--- src/plots/plot_doy.py
+++ src/plots/plot_doy.py
@@ -3,14 +3,22 @@
 DOY plot shows within-year variation in the data on the X axis and across-year
 variation in the data using a color ramp.
 """
-import matplotlib as mpl
-import mpl_toolkits.axes_grid1 as mpl_grid
+from datetime import datetime as dt
+import logging
+import os
 
+import matplotlib as mpl
+import matplotlib.cm
+import mpl_toolkits.axes_grid1 as mpl_grid
 import numpy as np
+import palettable
 
 from . import base_plot
-from tstools.ts_driver.ts_manager import tsm
-from tstools import settings
+from ..logger import qgis_log
+from ..ts_driver.ts_manager import tsm
+from .. import settings
+
+logger = logging.getLogger('tstools')
 
 
 class DOYPlot(base_plot.BasePlot):
@@ -19,157 +27,153 @@ class DOYPlot(base_plot.BasePlot):
         return "Stacked Day of Year Plot"
 
     def __init__(self, parent=None):
-        ### Setup datasets
-        # Actual data
-        self.x = np.zeros(0)
-        self.year = np.zeros(0)
-        self.y = np.zeros(0)
-        # Modeled fitted data
-        self.mx = np.zeros(0)
-        self.mx_year = np.zeros(0)
-        self.my = np.zeros(0)
-        # Location of pixel plotted
-        self.px = None
-        self.py = None
+        super(DOYPlot, self).__init__()
+        # Pixel location
+        self.title = ''
 
-        # Store colorbar so we know to delete
-        self.cbar = None
-        # Store range of data
-        self.yr_range = (0, 1)
+        # Colormap -- try to load from environment
+        cmap = os.environ.get('TSTOOLS_DOY_CMAP', 'perceptual_rainbow_16')
+        if hasattr(palettable.colorbrewer, cmap):
+            self.cmap = getattr(palettable.colorbrewer, cmap).mpl_colormap
+        elif hasattr(palettable.cubehelix, cmap):
+            self.cmap = getattr(palettable.cubehelix, cmap).mpl_colormap
+        else:
+            logger.error(
+                'Cannot find colormap {c}. Using backup'.format(c=cmap))
+            self.cmap = mpl.cm.cubehelix
 
-        # Setup plots
-        self.setup_plots()
         self.plot()
 
-    def update_plot(self):
-        """ Fetches new information and then calls plot
+    def _plot_series(self, idx, series, band, norm, mapper):
+        """ Plot a timeseries from a timeseries ts_driver
+
+        Args:
+          idx (int): index of all available plotting bands
+          series (int): index of series within timeseries driver
+          band (int): index of band within series within timeseries driver
+          norm (mpl.colors.Normalize): colormap normalizer
+          mapper (mpl.cm.ScalarMappable): scale mapper
+
         """
-        self.px, self.py = tsm.ts.get_px(), tsm.ts.get_py()
-        if self.px is not None and self.py is not None:
-            # Add + 1 so we index on 1,1 instead of 0,0 (as in ENVI/MATLAB)
-            self.px, self.py = self.px + 1, self.py + 1
+        for index, marker in zip(settings.plot_symbol[idx]['indices'],
+                                 settings.plot_symbol[idx]['markers']):
+            # Any points falling into this category?
+            if index.size == 0:
+                continue
 
-        self.x = np.array([int(d.strftime('%j')) for d in tsm.ts.dates])
-        self.year = np.array([d.year for d in tsm.ts.dates])
-        self.y = tsm.ts.get_data(settings.plot['mask'])[settings.plot['band'], :]
+            # Get data and extract DOY and year
+            x, y = tsm.ts.get_data(series, band,
+                                   mask=settings.plot['mask'],
+                                   indices=index)
 
-        if settings.plot['fit'] is True and tsm.ts.result is not None:
-            if len(tsm.ts.result) > 0:
-                self.mx, self.my = tsm.ts.get_prediction(settings.plot['band'])
-            else:
-                self.mx, self.my = (np.zeros(0), np.zeros(0))
+            doy = np.array([int(d.strftime('%j')) for d in x])
+            year = np.array([d.year for d in x])
 
-            self.mx_year = []
-            for _mx in self.mx:
-                self.mx_year.append(np.array([d.year for d in _mx]))
+            # Check for year range
+            year_in = np.where((year >= settings.plot['x_min']) &
+                                (year <= settings.plot['x_max']))[0]
 
-        if settings.plot['break'] is True and tsm.ts.result is not None:
-            if len(tsm.ts.result) > 1:
-                self.bx, self.by = tsm.ts.get_breaks(settings.plot['band'])
-            else:
-                self.bx, self.by = (np.zeros(0), np.zeros(0))
-        self.plot()
+            # Plot
+            self.axis_1.scatter(doy[year_in], y[year_in],
+                                cmap=self.cmap, c=year[year_in],
+                                norm=norm,
+                                marker='o', edgecolors='none', s=25,
+                                picker=settings.plot['picker_tol'])
+
+        # TODO: prediction & breaks
 
     def plot(self):
-        """ Matplotlib plot of time series stacked by DOY
-        """
-        self.axes.clear()
+        self.axis_1.clear()
 
-        title = 'Time series - row: {r} col: {c}'.format(
-            r=str(self.py), c=str(self.px))
-        self.axes.set_title(title)
+        # Setup axes
+        if tsm.ts:
+            self.axis_1.set_title(tsm.ts.pixel_pos)
 
-        self.axes.set_xlabel('Day of Year')
-        if tsm.ts is None:
-            self.axes.set_ylabel('Band')
+        self.axis_1.set_xlabel('Day of Year')
+        self.axis_1.set_ylabel('Value')
+
+        # Setup colormap
+        if tsm.ts:
+            yr_min, yr_max = float('inf'), float('-inf')
+            for series in tsm.ts.series:
+                year = np.array([d.year for d in series.images['date']])
+                if year.min() <= yr_min:
+                    yr_min = year.min()
+                if year.max() >= yr_max:
+                    yr_max = year.max()
         else:
-            self.axes.set_ylabel(tsm.ts.band_names[settings.plot['band']])
+            yr_min, yr_max = dt.today().year, dt.today().year + 1
 
-        self.axes.grid(True)
-        self.axes.set_ylim([settings.plot['min'][settings.plot['band']],
-                            settings.plot['max'][settings.plot['band']]])
-        self.axes.set_xlim(0, 366)
-
-        if settings.plot['xmin'] is not None \
-                and settings.plot['xmax'] is not None:
-            # Find array indexes for year range
-            self.yr_range = np.arange(
-                np.where(self.year >= settings.plot['xmin'])[0][0],
-                np.where(self.year <= settings.plot['xmax'])[0][-1])
-        else:
-            self.yr_range = np.arange(0, self.year.shape[0])
-
-        # Specify the year min and max
-        yr_min = 0
-        yr_max = 1
-        if len(self.year) > 0:
-            yr_min = self.year.min()
-            yr_max = self.year.max()
-
-        # Setup colormap and mapper
-        cmap = mpl.cm.get_cmap('jet')
         norm = mpl.colors.Normalize(vmin=yr_min, vmax=yr_max)
-        mapper = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+        mapper = mpl.cm.ScalarMappable(norm=norm, cmap=self.cmap)
 
-        # Plot
-        sp = self.axes.scatter(self.x[self.yr_range], self.y[self.yr_range],
-                               cmap=cmap, c=self.year[self.yr_range],
-                               norm=norm,
-                               marker='o', edgecolors='none', s=25,
-                               picker=settings.plot['picker_tol'])
+        added = np.where(settings.plot['y_axis_1_band'])[0]
+        if added.size > 0:
+            for _added in added:
+                _series = settings.plot_series[_added]
+                _band = settings.plot_band_indices[_added]
 
-        # Only put colorbar if we have data
-        if tsm.ts is not None:
-            # Setup layout to add space
-            # http://matplotlib.org/mpl_toolkits/axes_grid/users/overview.html#axesdivider
-            divider = mpl_grid.make_axes_locatable(self.axes)
-            cax = divider.append_axes('right', size='5%', pad=0.05)
-            # Reset colorbar so it doesn't overwrite itself...
-            if self.cbar is not None:
-                self.fig.delaxes(self.fig.axes[1])
-                self.fig.subplots_adjust(right=0.90)
-            self.cbar = self.fig.colorbar(sp, cax=cax)
+                self._plot_series(_added, _series, _band, norm, mapper)
 
-        if settings.plot['fit'] is True:
-            med_year = []
-            fit_plt = []
-            # Find median year and plot that result
-            for n, _yr in enumerate(self.mx_year):
-                # Make sure _yr is not empty array
-                if len(_yr) == 0:
-                    continue
-                # Determine median year
-                med = int(np.median(_yr))
-                # Make sure median year is in our current x-axis
-                if settings.plot['xmin'] > med or settings.plot['xmax'] < med:
-                    continue
-                med_year.append(med)
+        self.axis_1.set_xlim((1, 366))
+        self.axis_1.set_ylim(settings.plot['y_min'][0],
+                             settings.plot['y_max'][0])
 
-                # Determine line color
-                col = mapper.to_rgba(med)
-
-                # Get index from mx predicted data for median year
-                fit_range = np.arange(
-                    np.where(_yr == med)[0][0],
-                    np.where(_yr == med)[0][-1])
-
-                # Recreate as DOY
-                mx_doy = np.array([int(d.strftime('%j')) for d in
-                                   self.mx[n][fit_range]])
-
-                # Plot
-                seg, = self.axes.plot(mx_doy, self.my[n][fit_range],
-                                      color=col, linewidth=2)
-                fit_plt.append(seg)
-
-            if len(med_year) > 0:
-                self.axes.legend(fit_plt,
-                                 ['Fit {n}: {y}'.format(n=n + 1, y=y)
-                                  for n, y in enumerate(med_year)])
-
-        # Redraw
         self.fig.tight_layout()
         self.fig.canvas.draw()
+
+    #     # Only put colorbar if we have data
+    #     if tsm.ts is not None:
+    #         # Setup layout to add space
+    #         # http://matplotlib.org/mpl_toolkits/axes_grid/users/overview.html#axesdivider
+    #         divider = mpl_grid.make_axes_locatable(self.axes)
+    #         cax = divider.append_axes('right', size='5%', pad=0.05)
+    #         # Reset colorbar so it doesn't overwrite itself...
+    #         if self.cbar is not None:
+    #             self.fig.delaxes(self.fig.axes[1])
+    #             self.fig.subplots_adjust(right=0.90)
+    #         self.cbar = self.fig.colorbar(sp, cax=cax)
+
+    #     if settings.plot['fit'] is True:
+    #         med_year = []
+    #         fit_plt = []
+    #         # Find median year and plot that result
+    #         for n, _yr in enumerate(self.mx_year):
+    #             # Make sure _yr is not empty array
+    #             if len(_yr) == 0:
+    #                 continue
+    #             # Determine median year
+    #             med = int(np.median(_yr))
+    #             # Make sure median year is in our current x-axis
+    #             if settings.plot['xmin'] > med or settings.plot['xmax'] < med:
+    #                 continue
+    #             med_year.append(med)
+
+    #             # Determine line color
+    #             col = mapper.to_rgba(med)
+
+    #             # Get index from mx predicted data for median year
+    #             fit_range = np.arange(
+    #                 np.where(_yr == med)[0][0],
+    #                 np.where(_yr == med)[0][-1])
+
+    #             # Recreate as DOY
+    #             mx_doy = np.array([int(d.strftime('%j')) for d in
+    #                                self.mx[n][fit_range]])
+
+    #             # Plot
+    #             seg, = self.axes.plot(mx_doy, self.my[n][fit_range],
+    #                                   color=col, linewidth=2)
+    #             fit_plt.append(seg)
+
+    #         if len(med_year) > 0:
+    #             self.axes.legend(fit_plt,
+    #                              ['Fit {n}: {y}'.format(n=n + 1, y=y)
+    #                               for n, y in enumerate(med_year)])
+
+    #     # Redraw
+    #     self.fig.tight_layout()
+    #     self.fig.canvas.draw()
 
     def disconnect(self):
         pass
