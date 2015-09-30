42c198cb5a285baeaa2b4e0d0b4e3bd610e2ac24
Chris Holden <ceholden@gmail.com>
2015-09-30 12:25:43 -0400

    Parse date for met data using field sep not idx

--- src/ts_driver/timeseries_yatsm_met.py
+++ src/ts_driver/timeseries_yatsm_met.py
@@ -1,11 +1,13 @@
 """ Timeseries driver for Landsat timeseries with meteorological data
 """
+import datetime as dt
 import logging
 import os
 
 from . import ts_utils
 from .series import Series
 from .timeseries_yatsm import YATSMTimeSeries
+from ..logger import qgis_log
 
 logger = logging.getLogger('tstools')
 
@@ -17,18 +19,19 @@ class YATSMMetTimeSeries(YATSMTimeSeries):
     _met_location = 'PRISM'
     _met_types = ['ppt', 'tmin', 'tmax', 'tmean']
     _met_pattern = 'PRISM*.bil'
-    _met_date_index = [23, 29]
+    _met_date_sep = '_'
+    _met_date_sepno = 4
     _met_date_format = '%Y%m'
 
     config = [c for c in YATSMTimeSeries.config]
     config.extend([
         '_met_location', '_met_types', '_met_pattern',
-        '_met_date_index', '_met_date_format',
+        '_met_date_sep', '_met_date_sepno', '_met_date_format'
     ])
     config_names = [cn for cn in YATSMTimeSeries.config_names]
     config_names.extend([
         'Met root location', 'Met types', 'Met data pattern',
-        'Met date index', 'Met date format',
+        'Met filename separator', 'Met date index', 'Met date format'
     ])
 
     def __init__(self, location, config=None):
@@ -43,6 +46,21 @@ class YATSMMetTimeSeries(YATSMTimeSeries):
             images = ts_utils.find_files(
                 os.path.join(self._met_location, met_type),
                 self._met_pattern)
+
+            # Get date index from file
+            img = os.path.basename(images[0])
+            d = img.split(self._met_date_sep)[self._met_date_sepno]
+            try:
+                dt.datetime.strptime(d, self._met_date_format)
+            except Exception as e:
+                qgis_log('Could not parse date from %ith "%s"-separated field '
+                         'of filename %s using date format %s: %s' %
+                         (self._met_date_sepno, self._met_date_sep,
+                          img, self._met_date_format, e.message))
+                raise
+            idx_start = img.find(d)
+            self._met_date_index = (idx_start, idx_start + len(d))
+
             series = Series(
                 images,
                 self._met_date_index, self._met_date_format,
