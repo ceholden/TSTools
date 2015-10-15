3c3bde153d7949e5c58eef0ac8af0d1fe17e4e66
Chris Holden <ceholden@gmail.com>
2015-10-15 14:55:00 -0400

    Remove old figsave code

--- tstools/src/plots/base_plot.py
+++ tstools/src/plots/base_plot.py
@@ -42,37 +42,3 @@ class BasePlot(FigureCanvas):
 
     def update_plot(self):
         raise NotImplementedError('Subclass must implement `update_plot`')
-
-    def save_plot(self):
-        """ Save the matplotlib figure
-        """
-        # Parse options from settings
-        fname = settings.save_plot['fname']
-        fformat = settings.save_plot['format']
-        facecolor = settings.save_plot['facecolor']
-        edgecolor = settings.save_plot['edgecolor']
-        transparent = settings.save_plot['transparent']
-
-        # Format the output path
-        directory = os.path.split(fname)[0]
-
-        # Check for file extension
-        if '.' not in os.path.split(fname)[1]:
-            filename = '{f}.{e}'.format(f=os.path.split(fname)[1], e=fformat)
-
-        # Add in directory if none
-        if directory == '':
-            directory = '.'
-
-        # If directory does not exist, return False
-        if not os.path.exists(directory):
-            return False
-
-        # Join and save
-        filename = os.path.join(directory, filename)
-
-        self.fig.savefig(filename, format=fformat,
-                         facecolor=facecolor, edgecolor=edgecolor,
-                         transparent=transparent)
-
-        return True
